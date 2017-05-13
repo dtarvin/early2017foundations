6dc11389ea35ceebe53eb426fe69576f59e4a802
David R Tarvin Jr <david.tarvin@gmail.com>
2017-05-12 20:38:57 -0500

    take jQuery off index.html & put in script.js
    Put rejected jQuery in rejected.js

---
+++ davidTarvinEvaluation/rejected.js
@@ -0,0 +1,43 @@
+$("li").hover(function(){
+    var p=$(this).position;
+    
+});
+
+
+
+
+/* $("li").hover(function(){
+               $(this).css({
+                   margin-top, "48vh";
+                   background-image: url("images/pawprint.png");
+               });
+            }, function(){
+                $(this).css("margin-top", "52vh");
+            });*/
+          /*var paw=document.getElementById('paw');
+    var lis= document.getElementsByTagName('li');
+    window.onload=function(){
+        for(var i=0;i<lis.length;i++){
+	li=lis[i];
+	li.addEventListener('mouseover',function(){
+	    paw.style.left=Math.round(this.getBoundingClientRect().left)+"px";
+	});
+	li.addEventListener('mouseout',function(){
+	    paw.style.left="0px";
+	});
+    }};  */
+          /*function animateimg(){
+              $("li").function(){
+                  var x = $(this).offset().left;
+                  var y = $(this).offset().top;
+                  $("#paw").css("left", x);
+                  $(this).animate({
+                      left: x,
+                      top: y
+                  })
+              }
+          }
+            
+            $("li").hover(function(){
+                animateimg();
+            })*/
--- davidTarvinEvaluation/script.js
+++ davidTarvinEvaluation/script.js
@@ -1,43 +1,16 @@
-$("li").hover(function(){
-    var p=$(this).position;
-    
-});
-
-
-
-
-/* $("li").hover(function(){
-               $(this).css({
-                   margin-top, "48vh";
-                   background-image: url("images/pawprint.png");
-               });
-            }, function(){
-                $(this).css("margin-top", "52vh");
-            });*/
-          /*var paw=document.getElementById('paw');
-    var lis= document.getElementsByTagName('li');
-    window.onload=function(){
-        for(var i=0;i<lis.length;i++){
-	li=lis[i];
-	li.addEventListener('mouseover',function(){
-	    paw.style.left=Math.round(this.getBoundingClientRect().left)+"px";
-	});
-	li.addEventListener('mouseout',function(){
-	    paw.style.left="0px";
-	});
-    }};  */
-          /*function animateimg(){
-              $("li").function(){
-                  var x = $(this).offset().left;
-                  var y = $(this).offset().top;
-                  $("#paw").css("left", x);
-                  $(this).animate({
-                      left: x,
-                      top: y
-                  })
-              }
-          }
-            
-            $("li").hover(function(){
-                animateimg();
-            })*/
+$("li").mouseenter(function(){
+               var x = $(this).offset().left;
+                console.log("The left side is "+x);
+                var wth = $(this).width();
+                console.log("The width is "+wth);
+                var pawMove = x + (wth/2);
+                $("#paw").css("display", "inline");
+                $("#paw").animate({
+                   left: pawMove,
+                    "transform": "translate(-50%)"
+                }, 100, function(){
+                });
+                    
+                    
+                    //.css("left", pawMove);
+            });
